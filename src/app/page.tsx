9fafb75c676d548907e9123898ea9746a4ff3b96
Vinit <singhvinit435@gmail.com>
2025-03-20 14:53:21 +0530

    ui:hero-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Hero from "@/components/Hero";
+
+
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,39 @@
+import { Clock, Calendar } from "lucide-react";
+
+function Hero() {
+  return (
+    <div className="border border-blue-500 py-[4.5rem]">
+      <div className="w-[50%] mx-auto">
+        <div className="bg-indigo-400/30 flex items-center gap-x-1.5 w-fit py-1 px-2 mx-auto rounded-2xl">
+          <Clock className="w-4 h-4 text-indigo-500" />
+          <p className="text-sm font-medium text-indigo-500">
+            Never miss a coding contest again
+          </p>
+        </div>
+        <div className="py-5">
+          <h1 className="font-bold text-5xl text-center capitalize text-gray-700">
+            all coding contests.
+          </h1>
+          <h1 className="font-bold text-5xl text-center capitalize text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 to-indigo-700">
+            one platform.
+          </h1>
+        </div>
+        <p className="text-xl text-center text-gray-600">
+          Track competitive programming contests from Codeforces, LeetCode,
+          AtCoder and more. Stay updated with upcoming challenges.
+        </p>
+        <div className="flex items-center gap-x-4 justify-center py-6">
+          <button className="flex items-center gap-x-2 capitalize bg-indigo-500 text-lg text-white py-2 px-4 rounded-lg hover:bg-indigo-700 hover:shadow-2xl transition-all duration-200 ease-in">
+            <Calendar className="w-4 h-4" />
+            <span>view all contests</span>
+          </button>
+          <button className="flex items-center gap-x-2 capitalize bg-neutral-200 text-lg text-black py-2 px-4 rounded-lg hover:bg-neutral-400 hover:shadow-2xl transition-all duration-200 ease-in">
+            learn more
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
